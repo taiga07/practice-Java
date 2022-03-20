a85cbdd7ec9c785ccd7393c53211de462c372a6c
taiga07 <taigatani@me.com>
2022-03-20 22:51:06 +0900

    [practice]java

---
+++ Chapter04.java
@@ -0,0 +1,13 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name;
+		name = "谷太海";
+		System.out.println(name);
+
+		name = "こんにちは";
+		System.out.println(name);
+
+		final int NUMBER = 7;
+		System.out.println(NUMBER);
+	}
+}
---
+++ Chapter05.java
@@ -0,0 +1,20 @@
+public class Chapter05 {
+	public static void main(String[] args) {
+		long number1 = 123434444444444L;
+		double number2 = 0.1234;
+		boolean str = true;
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println(str);
+
+		Float number3 = 0.4321f;
+		Integer number4 = 4321;
+		Character char1 = 'あ';
+		System.out.println(number3);
+		System.out.println(number4);
+		System.out.println(char1);
+
+		int[] array1 = new int[] { 1, 2, 3, 4, 5 };
+		System.out.println(array1[3]);
+	}
+}
