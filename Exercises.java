df37db15f0c9440e0897c25698f6962b6c4735e8
taiga07 <taigatani@me.com>
2022-03-27 21:57:33 +0900

    [Java]練習問題

---
+++ Exercises.java
@@ -0,0 +1,44 @@
+// 北ソフト工房
+// 4.繰り返し
+
+public class Exercises {
+	public static void main(String[] args) {
+    // 4-1
+		for(int i = 1; i <= 10; i++) {
+			System.out.println("SPAM");
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+	}
+}
