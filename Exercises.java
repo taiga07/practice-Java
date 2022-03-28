94a447ccf602164aef18ee58265595525df320a8
taiga07 <taigatani@me.com>
2022-03-28 23:11:24 +0900

    [Java]nyuuryoku

--- Exercises.java
+++ Exercises.java
@@ -1,44 +1,45 @@
-// 北ソフト工房
-// 4.繰り返し
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Exercises {
-	public static void main(String[] args) {
-    // 4-1
-		for(int i = 1; i <= 10; i++) {
-			System.out.println("SPAM");
-		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+	public static void main(String[] args) throws IOException
+	{
+//		北ソフト工房
+//		2.入力
+		
+//		2-1
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int n = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+		System.out.println(s);
+		
+//		4.繰り返し
+		
+//		4-1
+//		for(int i = 1; i <= 10; i++) {
+//			System.out.println("SPAM");
+//		}
+		
+//		4-2
+//		for(int i = 1; i < 10; i++) {
+//			System.out.println(3 * i);
+//		}
+		
+//		4-3
+//		for( int i = 1, x = 2 ; i <= 8 ; i++, x *= 2 )
+//		    System.out.println( "2の" + i + "乗=" + x );
+		
+//		4-4
+//		int x = 1;
+//		for(int i = 1; i <= 7; i++)
+//			x *= i;
+//		System.out.println(x);
+		
+//		4-5
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int n = Integer.parseInt(br.readLine());
+		
+		
 	}
 }
