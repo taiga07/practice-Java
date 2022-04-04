54dfc687700c9ce7498c668f8fc6ee1a78fb9070
taiga07 <taigatani@me.com>
2022-04-04 21:28:36 +0900

    [java]9章確認問題

--- Chapter09.java
+++ Chapter09.java
@@ -6,7 +6,6 @@ public class Chapter09 {
 		double fuelCost = 12.5;
 //		残量
 		double fuelAmount = 55.0;
-		
 		Car car = new Car(fuelCost, fuelAmount);
 		car.move(20);
 		System.out.println("残量は、"+ car.getFuelAmount() +"です");
